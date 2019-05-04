8000b2538f7b0dc153d33a70374b7dadfe9f9494
matheuscenta <matheuscenta@gmail.com>
2019-05-04 22:26:20 +0200

    Added python solution for day 04/05

---
+++ may/04/p2.py
@@ -0,0 +1,24 @@
+def problem_2(arr):
+    n = len(arr)
+
+    left = [1 for _ in range(n)]
+    right = [1 for _ in range(n)]
+
+    for i in range(n-1):
+        left[i+1] = arr[i]*left[i]
+
+    for i in range(n-1, 0, -1):
+        right[i-1] = arr[i]*right[i]
+
+    return [left[i]*right[i] for i in range(n)]
+
+
+if __name__ == "__main__":
+    # read array from input
+    arr = list(map(int, input().split()))
+
+    # get the answer
+    ans = problem_2(arr)
+
+    # print the answer
+    print(ans)
