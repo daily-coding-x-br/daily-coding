33ac8e7a18eee889ac15e74e20cf71a978d7f046
Matheus Cariús <matheus.carius37@gmail.com>
2019-05-09 00:08:48 +0200

    Carius 07/05

---
+++ may/07/carius.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+auto cons(int a, int b) {
+    return [=](auto f) {return f(a,b);};
+}
+
+template<typename T>
+int car(T t) {
+    return t([=](int a, int b){return a;});
+}
+
+template<typename T>
+int cdr(T t) {
+    return t([=](int a, int b){return b;});
+}
+
+int main() {
+    auto t = cons(3,4);
+    cout << car(t) << endl;
+    cout << cdr(t) << endl;
+}
