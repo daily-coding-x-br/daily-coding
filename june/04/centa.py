3532deb57b19662dac6a0bfd2fbde8f01460fc2d
matheuscenta <matheuscenta@gmail.com>
2019-06-09 13:07:32 -0300

    Solution 04/05

--- june/04/centa.py
+++ june/04/centa.py
@@ -1,18 +1,34 @@
-from collections import heapq
+import heapq
 
 def running_medians(arr):
     smaller = []
     bigger = []
 
-    n = 0
     for num in arr:
-        if not bigger or num < bigger[0]:
-            heapq.heappush(smaller, -num)
-        else:
+        # Insert
+        if not bigger or num >= bigger[0]:
             heapq.heappush(bigger, num)
-        n += 1
-
-        if n % 2 == 0:
-            print("{:}".format((bigger[0]-smaller[0])/2))
         else:
-            
+            heapq.heappush(smaller, -num)
+
+        # Rebalance
+        if len(bigger) > len(smaller) + 1:
+            tmp = heapq.heappop(bigger)
+            heapq.heappush(smaller, -tmp)
+        elif len(smaller) > len(bigger) + 1:
+            tmp = -heapq.heappop(smaller)
+            heapq.heappush(bigger, tmp)
+
+        # Print current median
+        if len(smaller) > len(bigger):
+            print("{:}".format(-smaller[0]))
+        elif len(smaller) < len(bigger):
+            print("{:}".format(bigger[0]))
+        else:
+            print("{:}".format((bigger[0]-smaller[0])/2))
+
+
+if __name__ == "__main__":
+    arr = list(map(int, input().split()))
+
+    running_medians(arr)
