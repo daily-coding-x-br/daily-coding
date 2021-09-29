de6cb24b69a22759c05468fbf543b3c45ace11af
Roberto Moura <roberto@ffmoura.com>
2021-09-29 21:33:52 +0100

    correct may 09 solution

--- may/09/roberto.py
+++ may/09/roberto.py
@@ -1,8 +1,12 @@
 def countDecodings(s, d = {}, i = 0):
 	if (i in d):
 		return d[i]
-	if (i == len(s) - 1 or i == len(s)):
+	if (i == len(s)):
 		d[i] = 1
+	elif (i == len(s) + 1):
+		d[i] = 0
+	elif (int(s[i:i+1]) == 0):
+		d[i] = 0
 	elif (int(s[i:i + 2]) <= 26):
 		d[i] = countDecodings(s, d, i+1) + countDecodings(s, d, i+2)
 	else: #(int(s[i:i + 2]) > 26)
